108acd69310dfce710f786585e6b01c1f695d03e
efderosnay <e.fderosnay@gmail.com>
2019-06-20 12:15:12 +0100

    Update test file

--- src/BankAccountTest.java
+++ src/BankAccountTest.java
@@ -2,9 +2,22 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+    private static int count;
+
+    @org.junit.BeforeClass
+    public static void beforeClass(){
+        System.out.println("This executes before any test cases. Count = " + count++);
+    }
+
+    @org.junit.Before
+    public void setup(){
+        account = new BankAccount("Euge", "FDR", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test ...");
+    }
+
     @org.junit.Test
     public void deposit() throws Exception {
-        BankAccount account = new BankAccount("Euge", "FDR", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
         assertEquals(1200.00, account.getBalance(), 0);
@@ -17,24 +30,29 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance_deposit() throws Exception {
-        BankAccount account = new BankAccount("Euge", "FDR", 1000.00, BankAccount.CHECKING);
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() throws Exception {
-        BankAccount account = new BankAccount("Euge", "FDR", 1000.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true(){
-        BankAccount account = new BankAccount("Euge", "FDR", 1000.00 , BankAccount.CHECKING);
         assertTrue("The account is NOT a checking account", account.isChecking());
     }
 
+    @org.junit.AfterClass
+    public static void afterClass(){
+        System.out.println("This executes after any test cases. Count = " + count++);
+    }
 
+    @org.junit.After
+    public void teardown(){
+        System.out.println("Count = " + count++);
+    }
 
 }
